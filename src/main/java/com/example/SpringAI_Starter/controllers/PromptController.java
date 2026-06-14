2a2e84037e5bd81d3f7ebe2d1fdd4c6e9adea4e4
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-06-14 18:36:53 +0530

    Added few-shot prompting

--- src/main/java/com/example/SpringAI_Starter/controllers/PromptController.java
+++ src/main/java/com/example/SpringAI_Starter/controllers/PromptController.java
@@ -26,3 +26,27 @@ public class PromptController {
       return result;
 
   }
+
+  @GetMapping("/few-shot")
+  public String fewShot(@RequestParam String message){ // Accept a message as a query parameter
+      String result= chatClient.prompt()
+      .user(u -> u.text("""
+      Tell me the time complexity of the algorithm based on the name given
+      Only respond the time complexity or nothing else
+
+        Examples:
+        Algorithm: Bubble Sort
+        Time Complexity: O(n^2)
+        Algorithm: Merge Sort
+        Time Complexity: O(n log n)
+
+        Text: {algorithmName}
+
+          """).param("algorithmName", message))
+          .call()
+          .content(); 
+
+      return result;
+
+  }
+}
